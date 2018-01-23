b20db493ccc4cdbe48c10be0841a5603def058ae
bikeshrestha <srpbsm@gmail.com>
2018-01-23 10:28:16 +0545

    bootnode added

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -57,8 +57,8 @@ var RinkebyV5Bootnodes = []string{
 // TitanBootnodes are the enode URLs of the P2P bootstrap nodes running on the
 // Titan chain network.
 var TitanBootnodes = []string{
-	"enode://a24ac7c5484ef4ed0c5eb2d36620ba4e4aa13b8c84684e1b4aab0cebea2ae45cb4d375b77eab56516d34bfbd3c1a833fc51296ff084b770b94fb9028c4d25ccf@52.169.42.101:30303", // IE
-	"enode://343149e4feefa15d882d9fe4ac7d88f885bd05ebb735e547f12e12080a9fa07c8014ca6fd7f373123488102fe5e34111f8509cf0b7de3f5b44339c9f25e87cb8@52.3.158.184:30303",  // INFURA
+	"enode://4e9f0039a9ae15b1137f865cfb384fe4b87dfbde828dff45c667a2cb159a24bbfc28429bf3560ed147e4a5e3c309d270161a624416550973d255161e78d4df7f@192.168.1.6:30303", // bks
+
 
 
 }
@@ -67,8 +67,8 @@ var TitanBootnodes = []string{
 // Titan chain network for the experimental RLPx v5 topic-discovery network.
 var TitanV5Bootnodes = []string{
 
-	"enode://a24ac7c5484ef4ed0c5eb2d36620ba4e4aa13b8c84684e1b4aab0cebea2ae45cb4d375b77eab56516d34bfbd3c1a833fc51296ff084b770b94fb9028c4d25ccf@52.169.42.101:30303?discport=30304", // IE
-	"enode://343149e4feefa15d882d9fe4ac7d88f885bd05ebb735e547f12e12080a9fa07c8014ca6fd7f373123488102fe5e34111f8509cf0b7de3f5b44339c9f25e87cb8@52.3.158.184:30303?discport=30304",  // INFURA
+	"enode://4e9f0039a9ae15b1137f865cfb384fe4b87dfbde828dff45c667a2cb159a24bbfc28429bf3560ed147e4a5e3c309d270161a624416550973d255161e78d4df7f@192.168.1.6:30303?discport=30304", // IE
+
 
 }
 
